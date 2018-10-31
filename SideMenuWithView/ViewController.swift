72d80b02ae61767412272ae7afadd5e5716d87b9
Md. Mahadhi Hassan Chowdhury <Sunny@dream71s-imac.local>
2018-10-31 19:22:06 +0600

    completed navigation

--- SideMenuWithView/ViewController.swift
+++ SideMenuWithView/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leadingConstraintHome: NSLayoutConstraint!
+    @IBOutlet weak var trailingConstraintHome: NSLayoutConstraint!
+    
+    @IBOutlet weak var homeView: UIView!
+    
+    var showMenuFlag : Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        homeView.layer.shadowOpacity = 1
+        homeView.layer.shadowRadius = 30
+        
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        
+        leadingConstraintHome.constant = 0
+        trailingConstraintHome.constant = 0
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func showMenu(_ sender: Any) {
+       
+        if showMenuFlag {
+            leadingConstraintHome.constant = 0
+            trailingConstraintHome.constant = 0
+            
+        } else {
+            leadingConstraintHome.constant = self.view.bounds.width/2
+            trailingConstraintHome.constant = -(self.view.bounds.width/2)
+            
+        }
+        showMenuFlag = !showMenuFlag
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
 }
 
